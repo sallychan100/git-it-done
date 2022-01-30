24f756356740dd5d7ba83f09e841c4f11f11ede7
Sally <sallywing123@gmail.com>
2022-01-30 05:05:38 -0800

    search by user form updated

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,35 @@
-var getUserRepos = function() {
-  fetch("https://api.github.com/users/octocat/repos");
-  };
-  
-  getUserRepos();
+var getUserRepos = function(user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  });
+};
+getUserRepos();
+
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  console.log(event);
+  // get value from input element
+  // trim() method removes whitespace from both ends of a string and returns a new string
+  // value of nameInputEl DOM variable are stored at varaible 'username'
+var username = nameInputEl.value.trim();
+
+// check that there's a value in that username variable
+// If there is a value, pass input data to getUserRepos() as an argument. 
+if (username) {
+  getUserRepos(username);
+  nameInputEl.value = "";
+} else {
+  alert("Please enter a GitHub username");
+}
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
